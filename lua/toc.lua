2dcd6304c215536ac095e645dbae37e9aebaed6b
Yousef Akbar <yhakbar99@gmail.com>
2024-12-25 19:30:46 +0300

    Refactor into separate functions and general cleanup

--- lua/toc.lua
+++ lua/toc.lua
@@ -1,36 +1,26 @@
 local M = {}
--- local telescope = require "telescope.builtin"
 local v = vim.api
 
 M.setup = function()
-  vim.keymap.set("n", "<leader>mc", M.TOC)
   vim.cmd[[command Toc :lua require('toc').TOC()]]
 end
 
-M.TOC = function()
-  -- Get current buffer's contents
+-- `get_headings` scans and returns a table of headings found in `lines`
+local get_headings_cur_buf = function()
   local lines = v.nvim_buf_get_lines(0, 0, -1, false)
-
-  -- Extract headings (#+) using regex on all the lines
   local headings = {}
   for i, line in ipairs(lines) do
     local hlvl, htxt = string.match(line, "^(#+) (.*)$")
     if hlvl then
-      table.insert(headings, { title = htxt, line = i, level = #hlvl })
+      table.insert(headings, { title = htxt, line = i, level = hlvl })
     end
   end
+  return headings
+end
 
-  -- If no headings are found, return early
-  if #headings == 0 then
-    print("No headings found!")
-    return
-  end
-
-  -- Get the current window id for the markdown file
-  local markdown_win = vim.api.nvim_get_current_win()
-
+local dispatch_telescope_headings = function(headings, win)
   local opts = {
-    prompt_title = "Select Heading",
+    prompt_title = "📖 Select Heading",
     finder = require('telescope.finders').new_table({
       results = vim.tbl_map(function(heading) return heading.title end, headings)  -- Map titles to the results list
     }),
@@ -44,22 +34,35 @@ M.TOC = function()
         -- Find the selected heading in the headings list
         for _, heading in ipairs(headings) do
           if heading.title == heading_title then
-            print("Found title:", heading.title, "at line:", heading.line)
             -- Navigate to the selected heading
-            vim.api.nvim_win_set_cursor(markdown_win, { heading.line, 0 })
+            v.nvim_win_set_cursor(win, { heading.line, 0 })
             break
           end
         end
 
         actions.close(prompt_bufnr)
+        vim.cmd[[normal zz]]
       end)
       return true
     end,
   }
 
-  local pickers = require("telescope.pickers")
-  pickers.new(opts):find()
+  -- local pickers = require("telescope.pickers")
+  require("telescope.pickers").new(opts):find()
+end
 
+M.TOC = function()
+  -- Get current buffer's headings if any
+  local headings = get_headings_cur_buf()
+
+  -- If no headings are found, return early
+  if #headings == 0 then
+    print("No headings found!")
+    return
+  end
+
+  -- Dispatch the headings to Telescope for search and select
+  dispatch_telescope_headings(headings, v.nvim_get_current_win())
 end
 
 return M
